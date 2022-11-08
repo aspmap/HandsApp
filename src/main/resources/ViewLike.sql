e26a9d2f93dfc8e9bbba8320243ca09265b8ceae
apavlov <apavlov@systematica-consulting.ru>
2022-11-08 14:44:44 +0300

    Edit SQL

--- src/main/resources/ViewLike.sql
+++ src/main/resources/ViewLike.sql
@@ -14,6 +14,8 @@ select * from post p
             where u.username = 'shurrik77'
             order by p.created_at desc;
 
+
+
 --select count(p.post_id) from post p " +
  --           "join subscriptions s on s.user_sub_id = p.user_id " +
 --            "join users u on u.user_id = s.user_id " +
