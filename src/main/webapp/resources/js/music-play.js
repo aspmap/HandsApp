8ccdfe3356089f9dcc4b07597bfbb33dd0ec32df
aspmap <aspmap@yandex.ru>
2025-06-16 13:25:42 +0300

    Доработка раздела "Моя музыка"

--- src/main/webapp/resources/js/music-play.js
+++ src/main/webapp/resources/js/music-play.js
@@ -1,9 +1,7 @@
-function repeat() {
-    var selector = 'audio';
-    var audios = $(selector), len = audios.length, i = 0;
-    audios.bind('ended', function (e) {
-        i++;
-        if (i === len) i = 0;
-        audios[i].play();
-    });
-}
+var selector = 'audio';
+var audios = $(selector), len = audios.length, j = 0;
+audios.bind('ended', function (e) {
+    j++;
+    if (j === len) j = 0;
+    audios[j].play();
+});
--- src/main/webapp/resources/js/switch-track.js
+++ src/main/webapp/resources/js/switch-track.js
@@ -1,8 +1,8 @@
 var a = document.getElementsByTagName("audio");
 
-function other_pause(b){
-    for(i=0; i < a.length; i++){
-        if(!(i==b)){
+function switchTrack(b) {
+    for (i = 0; i < a.length; i++) {
+        if (!(i == b)) {
             a[i].pause()
             a[i].currentTime = 0;
         };
