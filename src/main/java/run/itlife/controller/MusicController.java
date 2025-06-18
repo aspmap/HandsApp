e76df772576541b0dcb5946c8ba6da742ebe745d
aspmap <aspmap@yandex.ru>
2025-06-18 10:05:43 +0300

    Рефакторинг

--- src/main/java/run/itlife/controller/MusicController.java
+++ src/main/java/run/itlife/controller/MusicController.java
@@ -46,7 +46,7 @@ public class MusicController {
     private static byte[] musicFile;
     @Autowired
     CommonsParams commonsParams;
-    private static final int MAX_UPLOAD_MUSIC_FILE_SIZE_IN_MB = 100 * 24 * 24; // 100 МБ
+    private static final int MAX_UPLOAD_MUSIC_FILE_SIZE_IN_MB = 100 * 1024 * 1024; // 100 МБ
     private static final Logger log = LoggerFactory.getLogger(MusicController.class);
 
     @Autowired
