d845e899d3e108136554ce210142e84935210307
aspmap <aspmap@yandex.ru>
2025-06-23 10:24:17 +0300

    Рефакторинг

--- src/main/java/run/itlife/controller/ApiController.java
+++ src/main/java/run/itlife/controller/ApiController.java
@@ -1,13 +1,18 @@
 package run.itlife.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import run.itlife.utils.CommonsParams;
 
 @Controller
 public class ApiController {
+    @Autowired
+    CommonsParams commonsParams;
     @GetMapping("/api")
     public String index(ModelMap modelMap) {
+        commonsParams.setCommonParams(modelMap);
         return "api/api";
     }
 }
